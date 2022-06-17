1605c480d7bdbd7f183085f21231474e90532fb5
nafisaaz <nafisaazzahra25@gmail.com>
2022-06-17 07:03:23 +0700

    crud firebase

---
+++ app/src/main/java/com/example/firebase/Siswa.java
@@ -0,0 +1,40 @@
+package com.example.firebase;
+
+public class Siswa {
+    String nim;
+    String nama_siswa;
+    String key;
+
+
+
+    public Siswa() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getNim() {
+
+        return nim;
+    }
+
+    public void setNim(String nim) {
+
+        this.nim = nim;
+    }
+
+    public String getNama_siswa() {
+
+        return nama_siswa;
+    }
+
+    public void setNama_siswa(String nama_siswa) {
+
+        this.nama_siswa = nama_siswa;
+    }
+}
---
+++ app/src/test/java/com/example/firebase/MyListDataTest.java
@@ -0,0 +1,7 @@
+package com.example.firebase;
+
+import junit.framework.TestCase;
+
+public class MyListDataTest extends TestCase {
+
+}
